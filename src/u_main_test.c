58eff8b67ed2bf4397055b2b16a517404c365f2c
Andreas Anderberg <andreas.anderberg@u-blox.com>
2023-10-31 14:30:29 +0100

    Cleanup

--- src/u_main_test.c
+++ src/u_main_test.c
@@ -25,18 +25,10 @@ int main(void)
 {
     uCxAtClient_t client;
 
-    char *s1;
-    char *s2;
-    int d1, d2;
     int len;
     uint8_t *pData;
     char buf[64];
     char rxBuf[1024];
-    strcpy(buf, "\"hej\",123,hopp,-100,10200a0b0c01");
-    int ret = uCxAtUtilParseParamsF(buf, "sdsdb", &s1, &d1, &s2, &d2, &len, &pData);
-    printf("ret: %d, s1: %s, d1: %d, s2: %s, d2: %d\n", ret, s1, d1, s2, d2);
-    printf("len: %d, pData: %02x%02x%02x%02x%02x%02x\n", len, pData[0], pData[1], pData[2], pData[3],
-           pData[4], pData[5]);
 
     uCxAtClientInit(NULL, rxBuf, sizeof(rxBuf), &client);
     uCxAtClientExecSimpleCmd(&client, "ATE0");
