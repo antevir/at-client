5741f1e96e6dd02d901142dda6041c931084c2a8
Andreas Anderberg <andreas.anderberg@u-blox.com>
2023-12-22 11:05:41 +0100

    u_cx_at_client: Refactor binary reception

--- inc/u_cx_at_client.h
+++ inc/u_cx_at_client.h
@@ -28,14 +28,14 @@ typedef void (*uUrcCallback_t)(struct uCxAtClient *pClient, void *pTag, char *pL
 
 
 typedef enum {
-    U_CX_AT_RX_STATE_CHARACTER,
-    U_CX_AT_RX_STATE_BINARY_RSP,
-    U_CX_AT_RX_STATE_BINARY_URC,
-    U_CX_AT_RX_STATE_BINARY_FLUSH
-} uCxAtRxState_t;
+    U_CX_BIN_STATE_BINARY_FLUSH,
+    U_CX_BIN_STATE_BINARY_RSP,
+    U_CX_BIN_STATE_BINARY_URC,
+} uCxAtBinaryState_t;
 
 typedef struct {
 
+    uCxAtBinaryState_t state;
     uint8_t rxHeaderCount;
     uint16_t remainingDataBytes;
     uint8_t *pBuffer;
@@ -61,7 +61,7 @@ typedef struct uCxAtClient {
     int32_t status;
     uUrcCallback_t urcCallback;
     void *pUrcCallbackTag;
-    uCxAtRxState_t rxState;
+    bool isBinaryRx;
     uCxAtBinaryRx_t binaryRx;
     uCxAtBinaryResponseBuf_t rspBinaryBuf;
     U_CX_MUTEX_HANDLE cmdMutex;
